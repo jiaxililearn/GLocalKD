08d9fd798143b2d3184b6ca72d3ddb646becb919
Jiaxi Li <jiaxili0319@gmail.com>
2023-01-09 14:43:09 +1100

    avoid storing all the graphs

--- graph_sampler.py
+++ graph_sampler.py
@@ -6,6 +6,7 @@ import torch.utils.data
 
 import util
 
+
 class GraphSampler(torch.utils.data.Dataset):
     ''' Sample graphs and nodes in graph
     '''
@@ -18,6 +19,9 @@ class GraphSampler(torch.utils.data.Dataset):
         self.assign_feat_all = []
         self.max_num_nodes = max_num_nodes
         self.sample_size = sample_size
+        self.graphs = graphs
+        self.g_index = g_index
+        self.features = features
 
         if features == 'default':
             self.feat_dim = util.node_dict(graphs[0])[0]['feat'].shape[0]
@@ -28,8 +32,10 @@ class GraphSampler(torch.utils.data.Dataset):
             graph_pool = G_list
 
         for G in tqdm(graph_pool):
+
+            # skip construction of additonal in-mem graphs
             if g_index is not None:
-                G = graphs[G]
+                break
 
             adj = np.array(nx.to_numpy_matrix(G))
             if normalize:
@@ -66,23 +72,68 @@ class GraphSampler(torch.utils.data.Dataset):
         return len(self.adj_all)
 
     def __getitem__(self, idx):
-        adj = self.adj_all[idx]
-        num_nodes = adj.shape[0]
-        if self.max_num_nodes > num_nodes:
-            adj_padded = np.zeros((self.max_num_nodes, self.max_num_nodes))
-            adj_padded[:num_nodes, :num_nodes] = adj
-        elif self.max_num_nodes < num_nodes:
-            degs = np.sum(np.array(adj), 1)
-            deg_index = np.argsort(degs, axis=0)
-            deg_ind = deg_index[0:num_nodes-self.max_num_nodes]
-            adj_padded = np.delete(adj, [deg_ind], axis=0)
-            adj_padded = np.delete(adj_padded, [deg_ind], axis=1)
+        if self.g_index is None:
+            adj = self.adj_all[idx]
+            num_nodes = adj.shape[0]
+            if self.max_num_nodes > num_nodes:
+                adj_padded = np.zeros((self.max_num_nodes, self.max_num_nodes))
+                adj_padded[:num_nodes, :num_nodes] = adj
+            elif self.max_num_nodes < num_nodes:
+                degs = np.sum(np.array(adj), 1)
+                deg_index = np.argsort(degs, axis=0)
+                deg_ind = deg_index[0:num_nodes-self.max_num_nodes]
+                adj_padded = np.delete(adj, [deg_ind], axis=0)
+                adj_padded = np.delete(adj_padded, [deg_ind], axis=1)
+            else:
+                adj_padded = adj
+                
+            return {'adj':adj_padded,
+                    'feats':self.feature_all[idx].copy(),
+                    'label':self.label_all[idx],
+                    'num_nodes': num_nodes,
+                    'assign_feats':self.assign_feat_all[idx].copy()}
         else:
-            adj_padded = adj
-               
-        return {'adj':adj_padded,
-                'feats':self.feature_all[idx].copy(),
-                'label':self.label_all[idx],
-                'num_nodes': num_nodes,
-                'assign_feats':self.assign_feat_all[idx].copy()}
+            _idx = self.g_index[idx]
+            G = self.graphs[_idx]
+            adj = np.array(nx.to_numpy_matrix(G))
+
+            num_nodes = adj.shape[0]
+            if self.max_num_nodes > num_nodes:
+                adj_padded = np.zeros((self.max_num_nodes, self.max_num_nodes))
+                adj_padded[:num_nodes, :num_nodes] = adj
+            elif self.max_num_nodes < num_nodes:
+                degs = np.sum(np.array(adj), 1)
+                deg_index = np.argsort(degs, axis=0)
+                deg_ind = deg_index[0:num_nodes-self.max_num_nodes]
+                adj_padded = np.delete(adj, [deg_ind], axis=0)
+                adj_padded = np.delete(adj_padded, [deg_ind], axis=1)
+            else:
+                adj_padded = adj
+            
+            # get structure feature
+            feat = None
+            if self.features == 'default':
+                f = np.zeros((self.max_num_nodes, self.feat_dim), dtype=float)
+                for i,u in enumerate(G.nodes()):
+                    f[i,:] = util.node_dict(G)[u]['feat']
+                feat = f
+            elif self.features == 'deg-num':
+                degs = np.sum(np.array(adj), 1)
+                if self.max_num_nodes > G.number_of_nodes():
+                    degs = np.expand_dims(np.pad(degs, (0, self.max_num_nodes - G.number_of_nodes()), 'constant', constant_values=0),
+                                      axis=1)
+                elif self.max_num_nodes < G.number_of_nodes():
+                    deg_index = np.argsort(degs, axis=0)
+                    deg_ind = deg_index[0: G.number_of_nodes()-self.max_num_nodes]
+                    degs = np.delete(degs, [deg_ind], axis=0)
+                    degs = np.expand_dims(degs, axis=1)
+                else:
+                    degs = np.expand_dims(degs, axis=1)
+                feat = degs
+
+            return {'adj': adj_padded,
+                    'feats': feat,
+                    'label': G.graph['label'],
+                    'num_nodes': num_nodes,
+                    'assign_feats': feat}
 
