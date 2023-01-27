e26c53c61adb6bca9ddd85ee2fa78d1eaeebe19b
Jiaxi Li <jiaxili0319@gmail.com>
2023-01-27 16:56:14 +1100

    update cpu compute

--- src/GCN_embedding.py
+++ src/GCN_embedding.py
@@ -95,9 +95,9 @@ class HetGraphConv(nn.Module):
         # print(f'node_types: {node_types}')
         # print(f'node_types shape: {node_types.shape}')
         het_y = []
-        # x = x.to(node_types.device)
-        # adj = adj.to(node_types.device)
-        # node_types = node_types.to(node_types.device)
+        x = x.to(node_types.device)
+        adj = adj.to(node_types.device)
+        node_types = node_types.to(node_types.device)
 
         for ntype in range(self.num_node_types):
             # print(f'==== Node Type {ntype} ====')
